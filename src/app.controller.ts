c65eb5b576e14f961bdf699a933ceae8c6a046f5
asuexe <vaghelavivek222222@gmail.com>
2025-02-10 21:44:31 +0530

    added cat route!

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
 
+
+@Controller('cats')
+export class CatsController {
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  findAll(): string {
+    return 'This action returns all cats';
   }
 }

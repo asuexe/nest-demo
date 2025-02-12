d1c9b5a41afcbd6f8fabeb4328dcc1acde557e33
asuexe <vaghelavivek222222@gmail.com>
2025-02-12 12:33:53 +0530

    added frontend URL in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:5173', // Allow requests from frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  await app.listen(3000);
 }
 bootstrap();

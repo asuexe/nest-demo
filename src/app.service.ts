b55bbd5610ccd0c555629a460ec201ac514b8ed4
asuexe <vaghelavivek222222@gmail.com>
2025-02-12 12:43:38 +0530

    some change

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   meow_meow(): string {
-    return 'Meowwwwwwwwwwwwwwwww';
+    return 'Meowwwwwwwwwwwww';
   }
 }
 
